5ff4e17ae1e4455dc7859c838e3b75dbc04c5440
Alexander Griffin <alexander_goudjeb@hotmail.com>
2019-06-04 20:09:21 +0200

    Noteskin Preview System

--- Scripts/RIO Rewrite.lua
+++ Scripts/RIO Rewrite.lua
@@ -2,11 +2,23 @@ ToBoolean = function(value) if value == "true" then return true else return fals
 
 RIO = {}
 
+RIO.Noteskin = 1
+RIO.NoteskinsEnabled = false
+RIO.NoteskinList = {}
+
 RIO.Config = function(Key,Value)
 	local Ret = Config.Load(Key,THEME:GetCurrentThemeDirectory().."/Config.ini")
 	if Ret then return Ret end
 	Config.Save(Key,Value,THEME:GetCurrentThemeDirectory().."/Config.ini")
 	return Value
+end
+
+for _,v in ipairs(NOTESKIN:GetNoteSkinNames()) do
+	local ToAdd = true
+	for DisabledSkin in string.gmatch(RIO.Config("DisabledNoteskins","delta-routine-p1,delta-routine-p2,cmd-routine-p1,cmd-routine-p2,routine-p1,routine-p2,rio-p1,rio-p2,rio-p3,rio-p4,rio-p5,perfor1,perfor2,perfor3,_disabled")..",", "(.-),") do
+		if v == DisabledSkin then ToAdd = false end
+	end
+	if ToAdd then RIO.NoteskinList[#RIO.NoteskinList+1] = v end
 end
 
 RIO.DoDebug = ToBoolean(RIO.Config("DebugMode","false"))
